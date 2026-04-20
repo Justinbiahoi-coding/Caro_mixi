10ab50194340482bb28040b213cff335b41550a3
Justinbiahoi-coding <thienbuideptrai2006@gmail.com>
2026-04-20 19:25:17 +0700

    them ui

--- GUI_Game.cpp
+++ GUI_Game.cpp
@@ -50,9 +50,8 @@ void UpdateGUIGame(GameState& game, UIState& ui) {
         if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT)) ui.endGameSelection = 0;
         if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) ui.endGameSelection = 1;
 
-        Rectangle btnPlayAgain = { 720, 560, 220, 50 };
-        Rectangle btnMenu = { 980, 560, 220, 50 };
-
+        Rectangle btnPlayAgain = { 960 - 240, 550, 220, 50 }; 
+        Rectangle btnMenu      = { 960 + 20,  550, 220, 50 };
         if (CheckCollisionPointRec(mouse, btnPlayAgain)) ui.endGameSelection = 0;
         if (CheckCollisionPointRec(mouse, btnMenu)) ui.endGameSelection = 1;
 
@@ -135,70 +134,55 @@ void DrawGUIGame(const GameState& game, const UIState& ui) {
         float cy = ui.cellStartY + game.cursorRow * ui.cellSize;
         DrawRectangleLinesEx({cx, cy, ui.cellSize, ui.cellSize}, 3.0f, DARKGREEN);
     }
-    DrawBadgeText(ui.mainFont, ui.titleBadge, "THONG TIN VAN DAU", 15, 402.3f, 84.7f, 48, WHITE);
+   // --- 1. THÔNG TIN VÁN ĐẤU (offsetY = 8.0f và 5.0f để đẩy chữ xuống cho khớp khung) ---
+    DrawBadgeText(ui.mainFont, ui.titleBadge, "THONG TIN VAN DAU", 15, 402.3f, 84.7f, 48, WHITE, 0.5f);
 
     char roundText[30];
     sprintf(roundText, "VONG CHOI %d", game.roundCount);
-    DrawBadgeText(ui.mainFont, ui.roundBadge, roundText, 100, 261.1f, 74.6f, 39, WHITE);
+    DrawBadgeText(ui.mainFont, ui.roundBadge, roundText, 100, 261.1f, 74.6f, 39, WHITE, -5.0f);
 
-    // if (game.matchStatus == 0) {
-    //     if (game.isPlayer1Turn) DrawTextCustom(ui.mainFont, TextFormat(">> LUOT CUA: %s (X) <<", game.player1.name), 790, 100, 30, RED);
-    //     else DrawTextCustom(ui.mainFont, TextFormat(">> LUOT CUA: %s (O) <<", game.player2.name), 790, 100, 30, BLUE);
-    // }
-
-    // if (game.inputType == 0) DrawTextCustom(ui.mainFont, "Dieu khien: Chuot", 880, 140, 20, GRAY);
-    // else DrawTextCustom(ui.mainFont, "Dieu khien: WASD + Enter", 850, 140, 20, GRAY);
-    
-    // DrawTextCustom(ui.mainFont, "Bam [L] de luu game | Nhan [M] de ve Menu", 720, 980, 25, WHITE);
     const char* guide = "Bam [L] de luu game | Nhan [M] de ve Menu";
     float guideSize = 25;
     float guideWidth = MeasureTextEx(ui.mainFont, guide, guideSize, 0).x;
-    float guideX = GetScreenWidth() / 2.0f - guideWidth / 2.0f;
+    float guideX = 1920.0f / 2.0f - guideWidth / 2.0f; 
     DrawTextCustom(ui.mainFont, guide, guideX, 1000, guideSize, WHITE);
 
-    // Kích thước cố định cho badge tên người chơi
+    // --- 2. THÔNG TIN NGƯỜI CHƠI ---
     int badgePWidth = 504;
     int badgePHeight = 109;
+    int nameFontSize = 55; // Giảm từ 72 xuống 55 để chữ không bị tràn viền
 
-    // --- Player 1 (Bên Trái) ---
-    float p1X = 31.2; 
-    float p1Y = 386.7 ;
-    // Vẽ badge dưới tên
+    // Player 1 (Bên Trái)
+    float p1X = 31.2f; 
+    float p1Y = 386.7f;
     DrawTexturePro(ui.playerBadge, {0, 0, (float)ui.playerBadge.width, (float)ui.playerBadge.height},
-                   {(float)p1X, (float)p1Y, (float)badgePWidth, (float)badgePHeight}, {0, 0}, 0.0f, WHITE);
+                   {p1X, p1Y, (float)badgePWidth, (float)badgePHeight}, {0, 0}, 0.0f, WHITE);
     
-    // Căn giữa text tên người chơi vào trong badge
-    int p1NameWidth = MeasureTextCustomX(ui.mainFont, game.player1.name, 72);
-    DrawTextCustom(ui.mainFont, game.player1.name, p1X + (badgePWidth - p1NameWidth) / 2, p1Y + 15, 72, WHITE);
+    int p1NameWidth = MeasureTextCustomX(ui.mainFont, game.player1.name, nameFontSize);
+    int p1NameHeight = MeasureTextCustomY(ui.mainFont, game.player1.name, nameFontSize);
+    float p1TextY = p1Y + (badgePHeight - p1NameHeight) / 2.0f - 5.0f; // Kéo chữ lên 5px cho cân
+    DrawTextCustom(ui.mainFont, game.player1.name, p1X + (badgePWidth - p1NameWidth) / 2.0f, p1TextY, nameFontSize, WHITE);
 
-
-    // DrawTextCustom(ui.mainFont, "Phe: X", p1X, p1Y + 70, 30, DARKGRAY);
-    // DrawTextCustom(ui.mainFont, TextFormat("Thang: %d | Thua: %d", game.player1.winCount, game.player1.loseCount), p1X, p1Y + 120, 30, BLACK);
-    // DrawTextCustom(ui.mainFont, TextFormat("So buoc van nay: %d", game.player1.stepCount), p1X, p1Y + 170, 25, GRAY);
-    
     if (game.gameMode == 1) {
-        DrawTextCustom(ui.mainFont, TextFormat("HP: %d / 3", game.player1.hp), p1X, p1Y + 220, 30, MAROON);
-        DrawTextCustom(ui.mainFont, TextFormat("Scan: %d", game.player1.scansLeft), p1X, p1Y + 270, 30, ORANGE);
+        DrawTextCustom(ui.mainFont, TextFormat("HP: %d / 3", game.player1.hp), p1X + 150, p1Y + 120, 30, MAROON);
+        DrawTextCustom(ui.mainFont, TextFormat("Scan: %d", game.player1.scansLeft), p1X + 150, p1Y + 170, 30, ORANGE);
     }
 
-    float p2X = 1388.4; 
-    float p2Y = 386.7 ;
-    // Vẽ badge dưới tên
+    // Player 2 (Bên Phải)
+    float p2X = 1388.4f; 
+    float p2Y = 386.7f;
     DrawTexturePro(ui.playerBadge, {0, 0, (float)ui.playerBadge.width, (float)ui.playerBadge.height},
-                   {(float)p2X, (float)p2Y, (float)badgePWidth, (float)badgePHeight}, {0, 0}, 0.0f, WHITE);
+                   {p2X, p2Y, (float)badgePWidth, (float)badgePHeight}, {0, 0}, 0.0f, WHITE);
     
-    // Căn giữa text tên người chơi vào trong badge
-    int p2NameWidth = MeasureTextCustomX(ui.mainFont, game.player2.name, 72);
-    DrawTextCustom(ui.mainFont, game.player2.name, p2X + (badgePWidth - p2NameWidth) / 2, p2Y + 15, 72, WHITE);
-    // DrawTextCustom(ui.mainFont, "Phe: O", p2X, p2Y + 70, 30, DARKGRAY);
-    // DrawTextCustom(ui.mainFont, TextFormat("Thang: %d | Thua: %d", game.player2.winCount, game.player2.loseCount), p2X, p2Y + 120, 30, BLACK);
-    // DrawTextCustom(ui.mainFont, TextFormat("So buoc van nay: %d", game.player2.stepCount), p2X, p2Y + 170, 25, GRAY);
+    int p2NameWidth = MeasureTextCustomX(ui.mainFont, game.player2.name, nameFontSize);
+    int p2NameHeight = MeasureTextCustomY(ui.mainFont, game.player2.name, nameFontSize);
+    float p2TextY = p2Y + (badgePHeight - p2NameHeight) / 2.0f - 5.0f; // Kéo chữ lên 5px cho cân
+    DrawTextCustom(ui.mainFont, game.player2.name, p2X + (badgePWidth - p2NameWidth) / 2.0f, p2TextY, nameFontSize, WHITE);
     
     if (game.gameMode == 1) {
-        DrawTextCustom(ui.mainFont, TextFormat("HP: %d / 3", game.player2.hp), p2X, p2Y + 220, 30, MAROON);
-        DrawTextCustom(ui.mainFont, TextFormat("Scan: %d", game.player2.scansLeft), p2X, p2Y + 270, 30, ORANGE);
+        DrawTextCustom(ui.mainFont, TextFormat("HP: %d / 3", game.player2.hp), p2X + 150, p2Y + 120, 30, MAROON);
+        DrawTextCustom(ui.mainFont, TextFormat("Scan: %d", game.player2.scansLeft), p2X + 150, p2Y + 170, 30, ORANGE);
     }
-    
     float P1charW = 356.0f, P1charH = 356.0f;
     float P2charW = 356.0f, P2charH = 356.0f;
 
@@ -217,27 +201,45 @@ void DrawGUIGame(const GameState& game, const UIState& ui) {
                 640.0f,
                 P2charW, P2charH,
                 true, p2Active);
-
     if (game.matchStatus != 0) {
-        DrawRectangle(ui.cellStartX, ui.cellStartY, BOARD_SIZE * ui.cellSize, BOARD_SIZE * ui.cellSize, Fade(WHITE, 0.7f));
+        // 1. Làm mờ bàn cờ
+        DrawRectangle((int)ui.cellStartX, (int)ui.cellStartY, BOARD_SIZE * (int)ui.cellSize, BOARD_SIZE * (int)ui.cellSize, Fade(WHITE, 0.7f));
         
-        if (game.matchStatus == 1) DrawTextCustom(ui.mainFont, TextFormat("%s (X) THANG!", game.player1.name), 800, 500, 50, RED);
-        if (game.matchStatus == 2) DrawTextCustom(ui.mainFont, TextFormat("%s (O) THANG!", game.player2.name), 800, 500, 50, BLUE);
-        if (game.matchStatus == 3) DrawTextCustom(ui.mainFont, "HOA NHAU!", 850, 500, 50, GRAY);
-        Rectangle btnPlayAgain = { 720, 560, 220, 50 };
-        Rectangle btnMenu = { 980, 560, 220, 50 };
+        // 2. Tự động đo độ dài chữ và Căn giữa thông báo Thắng/Thua
+        const char* winText = "";
+        Color winColor = WHITE;
+        if (game.matchStatus == 1) { winText = TextFormat("%s (X) THANG!", game.player1.name); winColor = RED; }
+        else if (game.matchStatus == 2) { winText = TextFormat("%s (O) THANG!", game.player2.name); winColor = BLUE; }
+        else if (game.matchStatus == 3) { winText = "HOA NHAU!"; winColor = GRAY; }
+        
+        int winWidth = MeasureTextCustomX(ui.mainFont, winText, 50);
+        DrawTextCustom(ui.mainFont, winText, 960 - winWidth / 2, 470, 50, winColor);
+
+        // 3. Vẽ Khung 2 nút bấm
+        Rectangle btnPlayAgain = { 960 - 240, 550, 220, 50 };
+        Rectangle btnMenu      = { 960 + 20,  550, 220, 50 };
 
         Color colorPlayAgain = (ui.endGameSelection == 0) ? YELLOW : LIGHTGRAY;
         Color colorMenu = (ui.endGameSelection == 1) ? YELLOW : LIGHTGRAY;
 
         DrawRectangleRec(btnPlayAgain, colorPlayAgain);
         DrawRectangleLinesEx(btnPlayAgain, 3.0f, BLACK);
-        DrawTextCustom(ui.mainFont, "Choi Van Moi", 745, 572, 25, BLACK);
 
         DrawRectangleRec(btnMenu, colorMenu);
         DrawRectangleLinesEx(btnMenu, 3.0f, BLACK);
-        DrawTextCustom(ui.mainFont, "Ve Menu", 1035, 572, 25, BLACK);
 
-        DrawTextCustom(ui.mainFont, "Dung [A]/[D] hoac Chuot de chon. [ENTER] de xac nhan.", 650, 640, 22, DARKGRAY);
+        // 4. Tự động căn giữa chữ bên TRONG từng nút bấm
+        const char* txtPlay = "Choi Van Moi";
+        const char* txtMenu = "Ve Menu";
+        int wPlay = MeasureTextCustomX(ui.mainFont, txtPlay, 25);
+        int wMenu = MeasureTextCustomX(ui.mainFont, txtMenu, 25);
+
+        DrawTextCustom(ui.mainFont, txtPlay, btnPlayAgain.x + (btnPlayAgain.width - wPlay) / 2, btnPlayAgain.y + 12, 25, BLACK);
+        DrawTextCustom(ui.mainFont, txtMenu, btnMenu.x + (btnMenu.width - wMenu) / 2, btnMenu.y + 12, 25, BLACK);
+
+        // 5. Căn giữa dòng Hướng dẫn thao tác
+        const char* guideEnd = "Dung [A]/[D] hoac Chuot de chon. [ENTER] de xac nhan.";
+        int guideEndW = MeasureTextCustomX(ui.mainFont, guideEnd, 22);
+        DrawTextCustom(ui.mainFont, guideEnd, 960 - guideEndW / 2, 620, 22, DARKGRAY);
     }
 }
--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 #include "GUI.h"
 
 int main() {
-    InitWindow(1920, 1080, "Do An Caro - Booming Mode");
+    // 1. Cho phép cửa sổ có thể kéo giãn (Resize) thoải mái
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
+    
+    // 2. Mở cửa sổ nhỏ hơn một chút cho vừa Mac (Ví dụ 1600x900)
+    InitWindow(1600, 900, "Do An Caro - Booming Mode");
     SetTargetFPS(60);
 
     SetExitKey(0);
@@ -14,17 +18,48 @@ int main() {
     UIState ui;
     InitGUI(ui); // put pic to ram
 
+    // 3. TẠO TẤM BẠT ẢO ĐÚNG CHUẨN 1920x1080
+    RenderTexture2D target = LoadRenderTexture(1920, 1080);
+    SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR); // Chống vỡ hạt khi thu nhỏ
+
     // Game loop
     while (!ui.shouldExit && !WindowShouldClose()) {
 
+        // --- BỘ TÍNH TOÁN THU PHÓNG (SCALE) ---
+        float scaleW = (float)GetScreenWidth() / 1920.0f;
+        float scaleH = (float)GetScreenHeight() / 1080.0f;
+        float scale = (scaleW < scaleH) ? scaleW : scaleH; // Giữ nguyên tỷ lệ 16:9
+
+        // Báo cho Raylib biết màn hình bị thu nhỏ để nó tính lại tọa độ Click chuột
+        SetMouseOffset(-(GetScreenWidth() - (1920.0f * scale)) * 0.5f, -(GetScreenHeight() - (1080.0f * scale)) * 0.5f);
+        SetMouseScale(1.0f / scale, 1.0f / scale);
+
         UpdateGUI(game, ui);
-        UpdateMusicStream(ui.bgMusic);
-        BeginDrawing();
+        
+        // --- BƯỚC A: VẼ MỌI THỨ LÊN TẤM BẠT ẢO ---
+        BeginTextureMode(target);
             ClearBackground(RAYWHITE);
             DrawGUI(game, ui);
+        EndTextureMode();
+
+        // --- BƯỚC B: IN TẤM BẠT ẢO LÊN MÀN HÌNH MÁY TÍNH ---
+        BeginDrawing();
+            ClearBackground(BLACK); // Viền đen bên ngoài nếu màn hình không phải 16:9
+
+            // Vẽ bạt ảo ra giữa màn hình (Lưu ý height của sourceRec phải là số âm do chuẩn OpenGL)
+            Rectangle sourceRec = { 0.0f, 0.0f, (float)target.texture.width, (float)-target.texture.height };
+            Rectangle destRec = { 
+                (GetScreenWidth() - (1920.0f * scale)) * 0.5f, 
+                (GetScreenHeight() - (1080.0f * scale)) * 0.5f, 
+                1920.0f * scale, 
+                1080.0f * scale 
+            };
+            DrawTexturePro(target.texture, sourceRec, destRec, { 0, 0 }, 0.0f, WHITE);
+
         EndDrawing();
     }
 
+    UnloadRenderTexture(target);
     UnloadGUI(ui);
     CloseWindow();
     
